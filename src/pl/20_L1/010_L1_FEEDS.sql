260b347e0689989daea90710bb0bb48e48fdb853
guerrillaanalytics <guerrillaanalytics@gmail.com>
2018-03-05 18:13:48 +0000

    added L1 cleaning script

---
+++ src/pl/20_L1/010_L1_FEEDS.sql
@@ -0,0 +1,8 @@
+﻿drop view if exists build_0_1_0.L1_feeds cascade;
+
+create view build_0_1_0.L1_feeds as
+select * from inputs.inp_001_feedbaby_01_feeds;
+
+/*
+select * from build_0_1_0.L1_feeds
+*/
