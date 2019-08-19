81735952e10ea9e34be68eb7add7ccefee292623
enda.ridge <nope@nope.co.uk>
2019-08-19 13:25:28 +0100

    tested formatting with black

--- setup.py
+++ setup.py
@@ -6,25 +6,21 @@ from setuptools import setup, find_packages
 
 THIS_DIR = Path(__file__)
 
-with open(THIS_DIR.joinpath('README.md')) as f:
+with open(THIS_DIR.joinpath("README.md")) as f:
     desc = f.read()
 
 setup(
-    name='<PROJECT_NAME>',
-    version='0.0.1',
-    url='<PROJECT_NAME_REPO_URL>',
-    packages=find_packages(include=['<PROJECT_NAME>', '<PROJECT_NAME>.*']),
-    author='',
-    author_email='',
-    description='',
+    name="<PROJECT_NAME>",
+    version="0.0.1",
+    url="<PROJECT_NAME_REPO_URL>",
+    packages=find_packages(include=["<PROJECT_NAME>", "<PROJECT_NAME>.*"]),
+    author="",
+    author_email="",
+    description="",
     long_description=desc,
-    install_requires=[
-        'pandas==0.24.2'
-    ],
+    install_requires=["pandas==0.24.2"],
     include_package_data=True,
-    platforms='any',
-    entry_points={
-        'console_scripts': [
-        ]
-    },
-    zip_safe=False)
+    platforms="any",
+    entry_points={"console_scripts": []},
+    zip_safe=False,
+)
